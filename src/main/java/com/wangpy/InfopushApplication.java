228d3b632b970ba4ceb3d1bf8a95991b1604d8cf
Star <318511992@qq.com>
2021-02-21 19:13:27 +0800

    Initial commit

---
+++ src/main/java/com/wangpy/InfopushApplication.java
@@ -0,0 +1,13 @@
+package com.wangpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfopushApplication.class, args);
+    }
+
+}
